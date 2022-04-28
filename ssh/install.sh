b65e4c5211d0582cfd553f1b29c3b0a818702f3c
Afonso <4775087+afonsograca@users.noreply.github.com>
2022-04-28 21:20:11 +0100

    :sparkles:(SSH): configure SSH

--- ssh/install.sh
+++ ssh/install.sh
@@ -24,29 +24,8 @@ handle_ssh() {
   print_header_footer "Step: SSH" $1
   
   if test "$dry_run" -eq 0; then
-    print_substep "NOT DRY RUN"
-    # ### Symlink config file
-    # symlink_files "$ssh_dir/config" "$HOME/.ssh" "" false
-    
-    # # Check if ssh-agent is running
-    # # TODO
-    
-    # ### Handle keys, if available
-    # for key_path in "caminho para as chaves"; do
-    #   # Symlink private key to the .ssh folder
-    #   symlink_files "$key_path" "$HOME/.ssh" "" false
-      
-    #   # TODO check if key is not public
-    #   if test "cenas"; then
-    #     # Add key to the ssh-agent
-    #     # TODO check if mac
-    #     if test "cenas"; then
-    #       ssh-add -K "$HOME/.ssh/$(basename "$key_path")"
-    #     else
-    #       ssh-add "$HOME/.ssh/$(basename "$key_path")"
-    #     fi
-    #   fi
-    # done
+    ### Symlink config file
+    symlink_files "$ssh_dir/config" "$HOME/.ssh" "" false
   fi
   
   ### Finishing touches
